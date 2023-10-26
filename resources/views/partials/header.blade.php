fcf202c44bec0f8d483e0655752930bdb58152cb
EsterDahri <esterdahri@gmail.com>
2023-10-26 21:07:55 +0200

    add bar in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,11 @@
 <header>
+    <div class="bar bg-primary py-2">
+        <div class="container d-flex justify-content-end text-white text-uppercase gap-4">
+            <div>dc power<span>&trade;<span>visa</span>&reg;</span></div>
+            <div>additional dc sites <span>&#9660;</span></div>
+        </div>
+    </div>
+
+    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
 
 </header>
